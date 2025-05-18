3f7cdaf96bd269f19fcce65d5dc2f9282216d427
Muhammad Fikri <fikri.muha28@gmail.com>
2025-05-18 13:20:26 +0700

    feat: add effect pinter to back details

--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -41,7 +41,7 @@ export default function Details() {
     <div className="max-w-3xl mx-auto p-4 sm:p-6 dark:bg-gray-900 dark:md:bg-transparent bg-white rounded-xl shadow-lg">
       <button
         onClick={() => navigate(-1)}
-        className="mb-6 inline-flex items-center space-x-2 text-blue-600 dark:text-cyan-400 hover:text-blue-400 transition"
+        className="mb-6 inline-flex items-center space-x-2 text-blue-600 dark:text-cyan-400 hover:text-blue-400 transition cursor-pointer"
       >
         <ArrowLeft />
         <span>Back</span>
